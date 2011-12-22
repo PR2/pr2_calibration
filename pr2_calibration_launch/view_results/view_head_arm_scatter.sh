5c071937636ab329943ca03379d9d4988eda7283
Michael Ferguson <mferguson@willowgarage.com>
2011-12-22 15:30:00 -0800

    update to latest visualization

--- pr2_calibration_launch/view_results/view_head_arm_scatter.sh
+++ pr2_calibration_launch/view_results/view_head_arm_scatter.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-rosrun calibration_estimation post_process.py /tmp/pr2_calibration/cal_measurements.bag /tmp/pr2_calibration/ `rospack find pr2_calibration_launch`/view_results/head_arm_config.yaml
+rosrun calibration_estimation error_visualization.py /tmp/pr2_calibration/cal_measurements.bag /tmp/pr2_calibration/ `rospack find pr2_calibration_launch`/view_results/head_arm_config.yaml
